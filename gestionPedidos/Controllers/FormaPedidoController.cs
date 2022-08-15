351fbe67580892642dda01a4e083cd4269003300
oscar <pandasoncode@gmail.com>
2022-08-15 17:47:09 +0200

    filtro formapedido y fechaenvio

---
+++ gestionPedidos/Controllers/FormaPedidoController.cs
@@ -0,0 +1,27 @@
+﻿using gestionPedidos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gestionPedidos.Controllers
+{
+    public class FormaPedidoController : Controller
+    {
+        public readonly gestionpedidoContext _gestionpedidoContext;
+
+        public FormaPedidoController(gestionpedidoContext gestionpedidoContext)
+        {
+            _gestionpedidoContext = gestionpedidoContext;
+        }
+
+        public IActionResult Index()
+        {
+            DateTime dateNow = new DateTime(2022, 07, 24);
+
+            var poees = _gestionpedidoContext.Poees.Where(p => p.Fechaenvio >= dateNow)
+                .OrderBy(p => p.Fechaenvio)
+                .ToList();
+
+
+            return View(poees);
+        }
+    }
+}
